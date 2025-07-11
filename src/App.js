e3410aa9fc3ab0c2c888354dd97d58ff347c96a4
Vitor Andre Beckenkamp <vitorandrebeckenkamp@outlook.com>
2025-07-11 08:02:54 -0300

    Merged cart feature

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
 
   return (
     <CartProvider>
-      <Cart/>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler}/>
       <main>
         <Meals />
